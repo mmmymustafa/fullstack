c2cb889ffd36e7df3370232e3c7348c8925daab8
Modini <mmmymustafa@yahoo.com>
2020-01-15 20:34:58 -0800

    dependent: destroy added

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ApplicationRecord
     validates :title, :artist_id, :about, :release_date, presence: true
 
     belongs_to :artist
-    has_many :tracks
+    has_many :tracks, dependent: :destroy
     has_one_attached :album_cover
     has_one_attached :track_banner
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,8 @@
 class Artist < ApplicationRecord
     validates :name, :about, presence: true
 
-    has_many :tracks
-    has_many :albums
+    has_many :tracks, dependent: :destroy
+    has_many :albums, dependent: :destroy
     has_one_attached :picture
     has_one_attached :banner_picture
 end
