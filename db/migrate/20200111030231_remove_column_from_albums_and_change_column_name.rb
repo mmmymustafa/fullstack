85434d6f83e033ec0fac15118b6db233999eadec
Modini <mmmymustafa@yahoo.com>
2020-01-15 20:44:03 -0800

    dependent: destroy added

--- db/migrate/20200111030231_remove_column_from_albums_and_change_column_name.rb
+++ db/migrate/20200111030231_remove_column_from_albums_and_change_column_name.rb
@@ -1,6 +1,5 @@
 class RemoveColumnFromAlbumsAndChangeColumnName < ActiveRecord::Migration[5.2]
   def change
-    remove_column :albums, :album_id
     rename_column :albums, :body, :about
   end
 end
