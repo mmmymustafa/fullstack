2e9e9e0533322995845a2ff4b604ae94cfd4cc1c
Modini <mmmymustafa@yahoo.com>
2020-03-29 14:15:45 -0700

    fixed minor css issues

--- frontend/components/index_page/index_page.jsx
+++ frontend/components/index_page/index_page.jsx
@@ -7,11 +7,17 @@ class IndexPage extends React.Component {
 
     render(){
         return(
-            <div className="index-page-container">
-                <div className="index-page">
-                    <News />
-                    <ChartsContainer />
+            <div>
+                <div className="index-page-container">
+                    <div className="index-page">
+                        <News />
+                        <ChartsContainer />
+                    </div>
+                </div >
+                <div>
+                    <div>
                     <Videos />
+                    </div>
                 </div>
             </div >
         )   
